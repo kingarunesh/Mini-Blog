1feed53abc8e6302306e3d69a0f3166f5cbbf5fd
Arunesh kumar <aruneshkumar.king@gmail.com>
2024-01-05 17:47:35 +0530

    4.2 : basic changes

--- index/views.py
+++ index/views.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from django.shortcuts import render
 
+from blog.models import Blog
 
 
 def index(request):
     year = datetime.now().year
     
     context = {
-        "year": year
+        "year": year,
+        "blogs": Blog.objects.all()
     }
     
     return render(request=request, template_name="index/index.html", context=context)
