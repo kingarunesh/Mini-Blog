3355e752c0ec49b37a542dff00b7179084740897
Arunesh kumar <aruneshkumar.king@gmail.com>
2024-01-06 00:42:06 +0530

    7.1 : blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from blog.models import Blog
+
 
 def blog(request):
-    return render(request=request, template_name="blog/blog.html")
+    
+    context = {
+        "blogs": Blog.objects.all()
+    }
+    
+    return render(request=request, template_name="blog/blog.html", context=context)
