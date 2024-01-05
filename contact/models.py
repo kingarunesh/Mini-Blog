6ce9866d223060172a7c865de1c0aaaabcf71ac4
Arunesh kumar <aruneshkumar.king@gmail.com>
2024-01-05 18:50:11 +0530

    6.1 : contact model added

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    phone = models.CharField(max_length=12)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    send_date = models.DateTimeField(auto_now_add=True)
