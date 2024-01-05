dc1724ef937eecd37d5834ef286f10753462807a
Arunesh kumar <aruneshkumar.king@gmail.com>
2024-01-05 18:56:45 +0530

    6.2 : contact send message

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+
+from contact.forms import ContactForm
+from contact.models import Contact
 
 
 def contact(request):
-    return render(request=request, template_name="contact/contact.html")
+    
+    form = ContactForm()
+    
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request=request, message="Your message send.")
+            
+            return HttpResponseRedirect("/")
+            
+    context = {
+        "form": form
+    }
+    
+    return render(request=request, template_name="contact/contact.html", context=context)
