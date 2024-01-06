298ab00c3870b714e605366974627aab15e82691
Arunesh kumar <aruneshkumar.king@gmail.com>
2024-01-06 20:10:32 +0530

    8.2 : auth -> login | logout | dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from dashboard.views import sign_up
+from dashboard.views import sign_up, login_view, dashboard, logout_view
 
 
 urlpatterns = [
-    path("sign-up/", view=sign_up, name="sign_up")
+    path("sign-up/", view=sign_up, name="sign_up"),
+    path("login/", view=login_view, name="login_view"),
+    path("dashboard/", view=dashboard, name="dashboard"),
+    path("logout/", view=logout_view, name="logout_view")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 from dashboard.forms import SignUpForm
 
 
 
-#SECTION :      sign up
+#SECTION :      dashboard
 
+def dashboard(request):
+    return render(request=request, template_name="dashboard/dashboard.html")
+
+
+
+
+#SECTION :      sign up
 def sign_up(request):
     
     form = SignUpForm()
@@ -21,3 +30,42 @@ def sign_up(request):
     }
     
     return render(request=request, template_name="dashboard/sign-up.html", context=context)
+
+
+#SECTION :      login
+def login_view(request):
+    form = AuthenticationForm()
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            
+            user = authenticate(username=username, password=password)
+            
+            login(request=request, user=user)
+            
+            
+            return HttpResponseRedirect("/dashboard/")
+            
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request=request, template_name="dashboard/login.html", context=context)
+
+
+
+
+
+
+#SECTION :      logout
+
+def logout_view(request):
+    logout(request=request)
+    
+    return HttpResponseRedirect("/login/")
